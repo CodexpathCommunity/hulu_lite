5675df6c2fb4edda12858a472f47a5bcca2310d1
Skiboybl <69096827+CodexpathCommunity@users.noreply.github.com>
2021-05-04 06:30:26 +0100

    added the scrollbar height

--- components/Header.js
+++ components/Header.js
@@ -13,7 +13,7 @@ function Header() {
   //https://links.papareact.com/ua6
   //images/hulusony.png
   return (
-    <div className="flex flex-col sm:flex-row m-5 jusfify-between items-center h-auto">
+    <div className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
       <div className="flex flex-grow justify-evenly max-w-2xl">
         <HeaderItem title="HOME" Icon={HomeIcon} />
         <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
