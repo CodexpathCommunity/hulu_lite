694a45e04c882e090c837f8e947abd02d6c141d9
Skiboybl <69096827+CodexpathCommunity@users.noreply.github.com>
2021-05-04 05:05:57 +0100

    added the header

--- components/Header.js
+++ components/Header.js
@@ -13,9 +13,14 @@ function Header() {
   //https://links.papareact.com/ua6
   //images/hulusony.png
   return (
-    <header className="">
-      <div className="">
-        <HeaderItem title="Home" Icon={HomeIcon} />
+    <div className="flex flex-col sm:flex-row m-5 jusfify-between items-center h-auto">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+        <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+        <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+        <HeaderItem title="SEARCH" Icon={SearchIcon} />
+        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
       </div>
       <Image
         src="/images/hulusony.png"
@@ -23,7 +28,7 @@ function Header() {
         height={100}
         className="object-contain"
       />
-    </header>
+    </div>
   );
 }
 
--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -1,9 +1,9 @@
 function HeaderItem({ Icon, title }) {
   return (
-    <div className="flex flex-col items-center cursor-pointer group w-12 sm:w-20">
+    <div className="flex flex-col items-center cursor-pointer group w-12 sm:w-20 hover:text-white">
       <Icon className="h-8 mb-1" />
       <p className="tracking-widest opacity-0 group-hover:opacity-100">
-        {title}{" "}
+        {title}
       </p>
     </div>
   );
